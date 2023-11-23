31b5865dfb14806e370dcf6ca9ad2d8b00c55e15
JacobJJoness <genjacob20@gmail.com>
2023-11-23 14:20:10 -0600

    Tailwind Works Through out Application
    Tailwind config file has been adjusted so that the app may use tailwind through it its structure and not just in the App.js file

---
+++ yum/screens/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+function Login() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-400">
+        <Text>
+            Login Here
+        </Text>
+    </View>
+  )
+}
+
+export default Login
