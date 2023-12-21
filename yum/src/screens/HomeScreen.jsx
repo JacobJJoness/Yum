b7ef069d2667f6bf91a88e8883d575f72df72ce2
Christian-Cormier <43218064+Christian-Cormier@users.noreply.github.com>
2023-12-21 11:49:16 -0600

    added RecieptReader.jsx, implemented tesseract,js and image capture

--- yum/src/screens/HomeScreen.jsx
+++ yum/src/screens/HomeScreen.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import TopNav from '../Components/TopNav';
 import BottomNav from '../Components/BottomNav';
+import RecieptReader from '../Components/RecieptReader';
 
 function HomeScreen() {
   return (
     <View className=" flex-1 justify-center items-center bg-green-100 ">
       <TopNav/>
       {/*  TopNav goes here*/}
-      {/*  This needs to be its own component for rendering recipes later*/}
+      {/* on button press run RecieptReader function for testing purposes */}
       <View className="h-4/6 mb-20">
 
       </View>
